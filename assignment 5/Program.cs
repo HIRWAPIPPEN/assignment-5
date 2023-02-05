44b88a484ba4c8bde9f5425c6d12570377da6466
HIRWAPIPPEN <119946786+HIRWAPIPPEN@users.noreply.github.com>
2023-02-05 20:10:54 +0100

    Add files via upload

---
+++ assignment 5/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+namespace Journal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 'start' to start writing into the journal, 'stop' to stop.");
+            string input = Console.ReadLine();
+            string journal = "";
+            bool writing = false;
+
+            while (input != "stop")
+            {
+                if (input == "start")
+                {
+                    writing = true;
+                }
+                else if (writing)
+                {
+                    journal += input + "\n";
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (journal.Length > 0)
+            {
+                string fileName = DateTime.Now.ToString("yyyy-MM-dd") + ".txt";
+                File.WriteAllText(fileName, "Captain's log\nStardate " + DateTime.Now.ToString("yyyy-MM-dd") + "\n" + journal + "Jean-Luc Picard");
+                Console.WriteLine("Journal written to file " + fileName);
+            }
+        }
+    }
+}
+
+
